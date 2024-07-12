53a68c9cc02b97fd7f137b50303c1b57eb3a27b5
kiterai <kiterai@foolslab.net>
2024-07-12 14:07:28 +0900

    texture

---
+++ include/brightcpp/internal/vulkan/texture.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <brightcpp/common.hpp>
+#include <brightcpp/internal/vulkan/vma.hpp>
+
+namespace BRIGHTCPP_NAMESPACE {
+
+namespace internal {
+
+struct texture_resource {
+    vma::UniqueImage image;
+    vma::UniqueAllocation allocation;
+    vk::UniqueImageView image_view;
+};
+
+texture_resource create_texture(vma::Allocator allocator, const uint8_t *data, uint32_t w, uint32_t h, vk::Queue queue, vk::CommandBuffer cmd_buf);
+
+} // namespace internal
+
+} // namespace BRIGHTCPP_NAMESPACE
--- src/vulkan/texture.cpp
+++ src/vulkan/texture.cpp
@@ -1,6 +1,32 @@
-#include <brightcpp/internal/vulkan/vma.hpp>
+#include <brightcpp/internal/vulkan/common.hpp>
+#include <brightcpp/internal/vulkan/texture.hpp>
 
-auto create_allocator(vk::Instance instance, vk::Device device) {
+namespace BRIGHTCPP_NAMESPACE {
+
+namespace internal {
+
+static auto create_sampler(vk::Device device) {
+    vk::SamplerCreateInfo create_info;
+    create_info.magFilter = vk::Filter::eNearest;
+    create_info.minFilter = vk::Filter::eNearest;
+    create_info.addressModeU = vk::SamplerAddressMode::eClampToEdge;
+    create_info.addressModeV = vk::SamplerAddressMode::eClampToEdge;
+    create_info.addressModeW = vk::SamplerAddressMode::eClampToEdge;
+    create_info.anisotropyEnable = false;
+    create_info.maxAnisotropy = 1.0f;
+    create_info.borderColor = vk::BorderColor::eFloatTransparentBlack;
+    create_info.unnormalizedCoordinates = false;
+    create_info.compareEnable = false;
+    create_info.compareOp = vk::CompareOp::eAlways;
+    create_info.mipmapMode = vk::SamplerMipmapMode::eLinear;
+    create_info.mipLodBias = 0.0f;
+    create_info.minLod = 0.0f;
+    create_info.maxLod = 0.0f;
+
+    return device.createSamplerUnique(create_info);
+}
+
+static auto create_allocator(vk::Instance instance, vk::Device device) {
     vma::AllocatorCreateInfo create_info;
     create_info.device = device;
     create_info.instance = instance;
@@ -8,7 +34,7 @@ auto create_allocator(vk::Instance instance, vk::Device device) {
     return vma::createAllocatorUnique(create_info);
 }
 
-auto cmd_change_image_layout(vk::CommandBuffer cmd_buf, vk::Image image, vk::ImageLayout old_layout, vk::ImageLayout new_layout) {
+static auto cmd_change_image_layout(vk::CommandBuffer cmd_buf, vk::Image image, vk::ImageLayout old_layout, vk::ImageLayout new_layout) {
     vk::ImageMemoryBarrier barrior;
     barrior.oldLayout = old_layout;
     barrior.newLayout = new_layout;
@@ -41,7 +67,7 @@ auto cmd_change_image_layout(vk::CommandBuffer cmd_buf, vk::Image image, vk::Ima
     cmd_buf.pipelineBarrier(src_stage, dst_stage, {}, {}, {}, {barrior});
 }
 
-auto cmd_copy_buffer_to_image(vk::CommandBuffer cmd_buf, vk::Buffer src_buf, vk::Image dst_image, uint32_t w, uint32_t h) {
+static auto cmd_copy_buffer_to_image(vk::CommandBuffer cmd_buf, vk::Buffer src_buf, vk::Image dst_image, uint32_t w, uint32_t h) {
     vk::BufferImageCopy imgCopyRegion;
     imgCopyRegion.bufferOffset = 0;
     imgCopyRegion.imageSubresource.aspectMask = vk::ImageAspectFlagBits::eColor;
@@ -56,7 +82,7 @@ auto cmd_copy_buffer_to_image(vk::CommandBuffer cmd_buf, vk::Buffer src_buf, vk:
     cmd_buf.copyBufferToImage(src_buf, dst_image, vk::ImageLayout::eTransferDstOptimal, imgCopyRegion);
 }
 
-auto create_empty_image(vma::Allocator allocator, const uint8_t *data, int w, int h, vk::ImageUsageFlags usage) {
+static auto create_empty_image(vma::Allocator allocator, const uint8_t *data, int w, int h, vk::ImageUsageFlags usage) {
     vk::ImageCreateInfo create_info;
     create_info.imageType = vk::ImageType::e2D;
     create_info.extent = vk::Extent3D(w, h, 1);
@@ -76,7 +102,7 @@ auto create_empty_image(vma::Allocator allocator, const uint8_t *data, int w, in
     return allocator.createImageUnique(create_info, allocation_info);
 }
 
-auto create_empty_buffer(vma::Allocator allocator, vk::DeviceSize size, vk::BufferUsageFlags usage) {
+static auto create_empty_buffer(vma::Allocator allocator, vk::DeviceSize size, vk::BufferUsageFlags usage) {
     vk::BufferCreateInfo create_info;
     create_info.size = size;
     create_info.usage = usage;
@@ -88,7 +114,7 @@ auto create_empty_buffer(vma::Allocator allocator, vk::DeviceSize size, vk::Buff
     return allocator.createBufferUnique(create_info, allocation_info);
 }
 
-auto create_filled_buffer(vma::Allocator allocator, const uint8_t *p_data, vk::DeviceSize size, vk::BufferUsageFlags usage) {
+static auto create_filled_buffer(vma::Allocator allocator, const uint8_t *p_data, vk::DeviceSize size, vk::BufferUsageFlags usage) {
     vk::BufferCreateInfo create_info;
     create_info.size = size;
     create_info.usage = usage;
@@ -107,36 +133,129 @@ auto create_filled_buffer(vma::Allocator allocator, const uint8_t *p_data, vk::D
     return buf;
 }
 
-auto create_texture(vma::Allocator allocator, const uint8_t *data, uint32_t w, uint32_t h, vk::Queue queue, vk::CommandBuffer cmd_buf) {
-    auto [tmp_buf, buf_allocation] =
-        create_filled_buffer(
-            allocator, data, w * h * 4,
-            vk::BufferUsageFlagBits::eTransferSrc);
+static auto create_descriptorset_layout(vk::Device device) {
+    auto bindings = {
+        vk::DescriptorSetLayoutBinding{}
+            .setBinding(0)
+            .setDescriptorType(vk::DescriptorType::eCombinedImageSampler)
+            .setDescriptorCount(1)
+            .setStageFlags(vk::ShaderStageFlagBits::eFragment),
+    };
 
-    auto [image, image_allocation] =
-        create_empty_image(
-            allocator, data, w, h,
-            vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eTransferDst);
+    vk::DescriptorSetLayoutCreateInfo create_info;
+    create_info.bindingCount = uint32_t(bindings.size());
+    create_info.pBindings = bindings.begin();
 
-    {
-        vk::CommandBufferBeginInfo cmd_begin_info;
-        cmd_begin_info.flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit;
+    return device.createDescriptorSetLayout(create_info);
+}
 
-        cmd_buf.begin(cmd_begin_info);
+static auto create_descriptor_pool(vk::Device device) {
+    auto pool_sizes = {
+        vk::DescriptorPoolSize{}
+            .setType(vk::DescriptorType::eCombinedImageSampler)
+            .setDescriptorCount(256),
+    };
 
-        cmd_change_image_layout(cmd_buf, image.get(), vk::ImageLayout::eUndefined, vk::ImageLayout::eTransferDstOptimal);
-        cmd_copy_buffer_to_image(cmd_buf, tmp_buf.get(), image.get(), w, h);
-        cmd_change_image_layout(cmd_buf, image.get(), vk::ImageLayout::eTransferDstOptimal, vk::ImageLayout::eShaderReadOnlyOptimal);
+    vk::DescriptorPoolCreateInfo create_info;
+    create_info.poolSizeCount = uint32_t(pool_sizes.size());
+    create_info.pPoolSizes = pool_sizes.begin();
+    create_info.maxSets = 256;
 
-        cmd_buf.end();
+    return device.createDescriptorPoolUnique(create_info);
+}
 
-        vk::SubmitInfo submit_info;
-        auto submit_cmd_buf = {cmd_buf};
-        submit_info.commandBufferCount = uint32_t(submit_cmd_buf.size());
-        submit_info.pCommandBuffers = submit_cmd_buf.begin();
+struct texture_factory {
+    vk::Device device;
+    vma::Allocator allocator;
+    vk::UniqueSampler sampler;
+    vk::UniqueDescriptorSetLayout desc_layout;
+    vk::UniqueDescriptorPool desc_pool;
+    vk::Queue queue;
+    vk::UniqueCommandPool cmd_pool;
+    vk::UniqueCommandBuffer cmd_buf;
 
-        queue.submit({submit_info});
+    texture_factory(vk::Device device, vma::Allocator allocator, const queue_index_set &queue_indices)
+        : device{device},
+          allocator{allocator},
+          sampler{create_sampler(device)},
+          desc_layout{create_descriptorset_layout(device)},
+          desc_pool{create_descriptor_pool(device)},
+          queue{device.getQueue(queue_indices.graphics_queue, 0)},
+          cmd_pool{create_cmd_pool(device, queue_indices, vk::CommandPoolCreateFlagBits::eTransient | vk::CommandPoolCreateFlagBits::eResetCommandBuffer)} {}
+
+    auto create_texture_descriptor_set(vk::ImageView image_view) {
+        auto layouts = {desc_layout.get()};
+
+        vk::DescriptorSetAllocateInfo allocate_info;
+        allocate_info.descriptorPool = desc_pool.get();
+        allocate_info.descriptorSetCount = uint32_t(layouts.size());
+        allocate_info.pSetLayouts = layouts.begin();
+
+        auto desc_set = std::move(device.allocateDescriptorSetsUnique(allocate_info)[0]);
+
+        auto images_info = {
+            vk::DescriptorImageInfo{}
+                .setImageView(image_view)
+                .setImageLayout(vk::ImageLayout::eShaderReadOnlyOptimal)
+                .setSampler(sampler.get()),
+        };
+
+        vk::WriteDescriptorSet write;
+        write.dstSet = desc_set.get();
+        write.descriptorType = vk::DescriptorType::eCombinedImageSampler;
+        write.dstBinding = 0;
+        write.dstArrayElement = 0;
+        write.descriptorCount = uint32_t(images_info.size());
+        write.pImageInfo = images_info.begin();
+
+        device.updateDescriptorSets({write}, {});
+
+        return desc_set;
     }
 
-    return std::make_pair(std::move(image), std::move(image_allocation));
-}
+    texture_resource create_texture(const uint8_t *data, uint32_t w, uint32_t h) {
+        auto [tmp_buf, buf_allocation] =
+            create_filled_buffer(
+                allocator, data, w * h * 4,
+                vk::BufferUsageFlagBits::eTransferSrc);
+
+        auto [image, image_allocation] =
+            create_empty_image(
+                allocator, data, w, h,
+                vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eTransferDst);
+
+        {
+            vk::CommandBufferBeginInfo cmd_begin_info;
+            cmd_begin_info.flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit;
+
+            cmd_buf->begin(cmd_begin_info);
+
+            cmd_change_image_layout(cmd_buf.get(), image.get(), vk::ImageLayout::eUndefined, vk::ImageLayout::eTransferDstOptimal);
+            cmd_copy_buffer_to_image(cmd_buf.get(), tmp_buf.get(), image.get(), w, h);
+            cmd_change_image_layout(cmd_buf.get(), image.get(), vk::ImageLayout::eTransferDstOptimal, vk::ImageLayout::eShaderReadOnlyOptimal);
+
+            cmd_buf->end();
+
+            vk::SubmitInfo submit_info;
+            auto submit_cmd_buf = {cmd_buf.get()};
+            submit_info.commandBufferCount = uint32_t(submit_cmd_buf.size());
+            submit_info.pCommandBuffers = submit_cmd_buf.begin();
+
+            queue.submit({submit_info});
+        }
+
+        auto image_view = create_image_view(device, image.get(), vk::Format::eR8G8B8A8Unorm);
+
+        auto desc_set = create_texture_descriptor_set(image_view.get());
+
+        return {
+            .image = std::move(image),
+            .allocation = std::move(image_allocation),
+            .image_view = std::move(image_view),
+        };
+    }
+};
+
+} // namespace internal
+
+} // namespace BRIGHTCPP_NAMESPACE
