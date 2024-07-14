351e41b0da39b8677c826509d7596817ee47bff6
kiterai <kiterai@foolslab.net>
2024-07-15 00:21:37 +0900

    simplify

--- include/brightcpp/internal/vulkan/vma.hpp
+++ include/brightcpp/internal/vulkan/vma.hpp
@@ -1,6 +1,3 @@
 #pragma once
 
-// to deal with mistake of include path in vk_mem_alloc.hpp
-#include <vma/vk_mem_alloc.h>
-
-#include <vulkan-memory-allocator-hpp/vk_mem_alloc.hpp>
+#include <vk_mem_alloc.hpp>
