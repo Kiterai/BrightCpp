380073ac4fa9eb501a828e2f69a7c508e9f81550
kiterai <kiterai@foolslab.net>
2024-08-05 16:37:24 +0900

    graphics interface

--- include/brightcpp/internal/interfaces/graphics.hpp
+++ include/brightcpp/internal/interfaces/graphics.hpp
@@ -1,4 +1,6 @@
 #include <brightcpp/common.hpp>
+#include <brightcpp/internal/interfaces/os_util.hpp>
+#include <brightcpp/internal/rendering.hpp>
 #include <cstdint>
 
 namespace BRIGHTCPP_NAMESPACE {
@@ -9,21 +11,17 @@ class texture_backend {
   public:
 };
 
-class screen_backend {
-  public:
-};
-
 class render_target_backend {
   public:
 };
 
 class graphics_backend {
   public:
-    virtual render_target_backend create_render_target(screen_backend screen) = 0;
-    virtual void destroy_render_target(render_target_backend rt) noexcept = 0;
-    virtual void set_current_render_target(render_target_backend rt) = 0;
+    virtual std::unique_ptr<render_target_backend> create_render_target(window_backend &screen) = 0;
+    virtual void destroy_render_target(render_target_backend &rt) noexcept = 0;
+    virtual void set_current_render_target(render_target_backend &rt) = 0;
 
-    virtual void draw(texture_backend texture) = 0;
+    virtual void draw(texture_backend texture, render_rect_info &info) = 0;
 };
 
 class texture_factory_backend {
@@ -32,6 +30,6 @@ class texture_factory_backend {
     virtual void destroy(texture_backend image) noexcept = 0;
 };
 
-}  // namespace internal
+} // namespace internal
 
-}  // namespace BRIGHTCPP_NAMESPACE
+} // namespace BRIGHTCPP_NAMESPACE
