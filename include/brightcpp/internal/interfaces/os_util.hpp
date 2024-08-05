125b6fad3b6e9ec75a9b1714c9c479de3f1a9199
kiterai <kiterai@foolslab.net>
2024-08-05 16:32:49 +0900

    os_util

---
+++ include/brightcpp/internal/interfaces/os_util.hpp
@@ -0,0 +1,28 @@
+#include <brightcpp/common.hpp>
+#include <brightcpp/window.hpp>
+#include <optional>
+#include <vulkan/vulkan.hpp>
+
+namespace BRIGHTCPP_NAMESPACE {
+
+namespace internal {
+
+class window_backend {
+    virtual ~window_backend() = default;
+
+    virtual vk::UniqueSurfaceKHR get_vulkan_surface(vk::Instance instance) {
+        throw std::exception("not implemented get_vulkan_surface()");
+    }
+    virtual bool is_close_requested() = 0;
+};
+
+class os_util_backend {
+  public:
+    virtual ~os_util_backend() = default;
+
+    virtual std::unique_ptr<window_backend> create_window(window::settings settings) = 0;
+};
+
+} // namespace internal
+
+} // namespace BRIGHTCPP_NAMESPACE
