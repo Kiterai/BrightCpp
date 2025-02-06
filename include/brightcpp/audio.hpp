5cf4d568660b3eb11fd70b9fddf3905b2912fae5
kiterai <kiterai@foolslab.net>
2025-02-06 16:32:17 +0900

    audio api

--- include/brightcpp/audio.hpp
+++ include/brightcpp/audio.hpp
@@ -7,71 +7,28 @@
 
 BRIGHTCPP_START
 
-struct audio_sample {
-}; 
-
-class audio_buffer {
-  public:
-    void write(audio_sample *samples, size_t length);
-    void read(audio_sample *samples, size_t length);
-    void cread(audio_sample *samples, size_t length, size_t pos = 0) const;
-    size_t len_samples() const;
-    size_t len_bytes() const;
+enum class audio_file_type {
+    auto_detect,
+    wav,
+    ogg,
+    mp3,
+    flac,
 };
 
-class audio_out {
+class audio : public handle_holder<audio> {
+  public:
+    audio(char *path, audio_file_type type = audio_file_type::auto_detect);
 };
 
-class audio_filter {
+class audio_player : public handle_holder<audio> {
   public:
-};
-
-class audio_impl;
-
-class audio_clip : handle_holder<audio_impl> {
-    size_t sample_begin, sample_end;
-
-  public:
-};
-
-class audio : handle_holder<audio_impl> {
-  public:
-    audio_clip clip(std::chrono::nanoseconds start);
-    audio_clip clip(std::chrono::nanoseconds start, std::chrono::nanoseconds end);
-};
-
-// class audio_streaming : handle_holder<audio_streaming> {
-//     audio_buffer stream_buf;
-
-//   public:
-//     audio_buffer &buffer() { return stream_buf; }
-
-//     void play();
-//     void pause();
-//     void resume();
-//     void stop();
-//     void seek(std::chrono::nanoseconds point);
-//     std::chrono::nanoseconds pos() const;
-
-//     bool loading() const;
-// };
-
-class audio_player {
-  public:
-    audio_player(audio_buffer &output);
+    audio_player(audio &data);
 
     void set(audio &);
-    void set(audio_clip &);
-    // void set(audio_streaming &);
-    // void set(audio_buffer &buf);
     void reset();
 
     audio_player &play_once();
-    audio_player &play_loop(std::chrono::nanoseconds loop_point = std::chrono::nanoseconds(0)) {
-    }
-
-    audio_filter &filter();
-    audio_filter &filter(audio_filter &filter);
+    audio_player &play_loop(std::chrono::nanoseconds loop_point = std::chrono::nanoseconds(0));
 
     void pause();
     void resume();
@@ -81,26 +38,17 @@ class audio_player {
     bool busy() const;
 };
 
-// class audio_player_multi {
-//     std::vector<audio_player> players;
+class audio_player_multi {
+  public:
+    audio_player &play_once(audio &);
 
-//   public:
-//     audio_player_multi(audio_buffer &output, size_t num);
-
-//     audio_player &play_once(audio &);
-//     audio_player &play_once(audio_clip &);
-//     audio_player &bind(audio_buffer &buf);
-
-//     audio_filter &filter();
-//     audio_filter &filter(audio_filter &filter);
-
-//     void pause();
-//     void resume();
-//     void stop();
-//     std::vector<std::chrono::nanoseconds> pos() const;
-//     std::vector<bool> busy() const;
-//     bool busy_all() const;
-//     bool busy_any() const;
-// };
+    void pause();
+    void resume();
+    void stop();
+    std::vector<std::chrono::nanoseconds> pos() const;
+    std::vector<bool> busy() const;
+    bool busy_all() const;
+    bool busy_any() const;
+};
 
 BRIGHTCPP_END
--- src/audio.cpp
+++ src/audio.cpp
@@ -1,5 +1,133 @@
+#include "audio.hpp"
+#include "global_module.hpp"
+#include "interfaces/audio.hpp"
 #include <brightcpp/audio.hpp>
+#include <unordered_map>
 
 BRIGHTCPP_START
+
+using g_audio = internal::global_module<internal::audio_backend>;
+
+std::vector<std::vector<float>> buffers;
+
+handle_holder<audio> easy_audio_loader(char *path, audio_file_type type) {
+    // TODO
+}
+
+audio::audio(char *path, audio_file_type type) : handle_holder{easy_audio_loader(path, type)} {}
+
+class audio_player_impl {
+    std::optional<audio> data;
+    internal::audio_context_id context_id;
+
+  public:
+    audio_player_impl() {
+        context_id = g_audio::get().play_audio_buffer(
+            internal::audio_buffer_play_info{
+                .delay_timer = 0,
+                // .current_pos = buffers[audio_handle].data(),
+                // .end_pos = buffers[audio_handle].data() + buffers[audio_handle].size(),
+                // .loop_pos = buffers[audio_handle].data(),
+                // .next_loop_end_pos = buffers[audio_handle].data() + buffers[audio_handle].size(),
+                .volume = 1.0f,
+                .mode = internal::audio_buffer_play_info::play_mode::normal,
+                .stopped = false,
+                .paused = true,
+            });
+    }
+    void set(audio &new_data) {
+        data = new_data;
+    }
+    void play_once() {
+        auto audio_handle = data->handle();
+
+        g_audio::get().set_playing_state(
+            context_id,
+            internal::audio_buffer_play_info{
+                .delay_timer = 0,
+                .current_pos = buffers[audio_handle].data(),
+                .end_pos = buffers[audio_handle].data() + buffers[audio_handle].size(),
+                .loop_pos = buffers[audio_handle].data(),
+                .next_loop_end_pos = buffers[audio_handle].data() + buffers[audio_handle].size(),
+                .volume = 1.0f,
+                .mode = internal::audio_buffer_play_info::play_mode::normal,
+                .stopped = false,
+                .paused = false,
+            });
+    }
+    void play_loop() {
+        auto audio_handle = data->handle();
+
+        g_audio::get().set_playing_state(
+            context_id,
+            internal::audio_buffer_play_info{
+                .delay_timer = 0,
+                .current_pos = buffers[audio_handle].data(),
+                .end_pos = buffers[audio_handle].data() + buffers[audio_handle].size(),
+                .loop_pos = buffers[audio_handle].data(),
+                .next_loop_end_pos = buffers[audio_handle].data() + buffers[audio_handle].size(),
+                .volume = 1.0f,
+                .mode = internal::audio_buffer_play_info::play_mode::loop,
+                .stopped = false,
+                .paused = false,
+            });
+    }
+};
+
+handle_holder<audio>::handle_value_t player_serial_count = 0;
+std::unordered_map<handle_holder<audio>::handle_value_t, audio_player_impl> players;
+
+handle_holder<audio>::handle_value_t player_register() {
+    players.emplace(player_serial_count, audio_player_impl{});
+    player_serial_count++;
+}
+
+audio_player::audio_player(audio &data) : handle_holder{player_register()} {
+    players[handle()].set(data);
+}
+
+void audio_player::set(audio &data) {
+    players[handle()].set(data);
+}
+
+void audio_player::reset() {
+    // TODO
+}
+
+audio_player &audio_player::play_once() {
+    players[handle()].play_once();
+    return *this;
+}
+
+audio_player &audio_player::play_loop(std::chrono::nanoseconds loop_point) {
+    players[handle()].play_loop();
+    return *this;
+}
+
+void audio_player::pause() {
+    // TODO
+}
+
+void audio_player::resume() {
+    // TODO
+}
+
+void audio_player::stop() {
+    // TODO
+}
+
+void audio_player::seek(std::chrono::nanoseconds point) {
+    // TODO
+}
+
+std::chrono::nanoseconds audio_player::pos() const {
+    // TODO
+    return std::chrono::nanoseconds(0);
+}
+
+bool audio_player::busy() const {
+    // TODO
+    return false;
+}
 
 BRIGHTCPP_END
