f4eaa00a08311592ee83400137a2ba157c2d0e5d
kiterai <kiterai@foolslab.net>
2025-02-10 17:39:47 +0900

    erased comments

--- src/libsoundio/audio.cpp
+++ src/libsoundio/audio.cpp
@@ -280,48 +280,12 @@ class audio_libsoundio : public audio_backend {
             global_module<audio_mixer>::get().read(buf, writable_frame_count);
 
             for (int frame = 0; frame < writable_frame_count; frame++) {
-                // float sample = 0;
-                // for (auto &playing : thiz->playing_list) {
-                //     if (playing.paused)
-                //         continue;
-                //     sample += *playing.current_pos;
-                //     playing.current_pos++;
-                //     if (playing.current_pos >= playing.end_pos) {
-                //         switch (playing.mode) {
-                //         case audio_buffer_play_info::play_mode::normal:
-                //             playing.current_pos = playing.loop_pos;
-                //             playing.paused = true;
-                //             break;
-                //         case audio_buffer_play_info::play_mode::oneshot:
-                //             playing.stopped = playing.paused = true;
-                //             break;
-                //         case audio_buffer_play_info::play_mode::loop:
-                //             playing.current_pos = playing.loop_pos;
-                //             playing.end_pos = playing.next_loop_end_pos;
-                //             break;
-                //         case audio_buffer_play_info::play_mode::streaming_loop_invalid:
-                //             playing.paused = true;
-                //             break;
-                //         case audio_buffer_play_info::play_mode::streaming_loop_available:
-                //             playing.current_pos = playing.loop_pos;
-                //             playing.end_pos = playing.next_loop_end_pos;
-                //             playing.mode = audio_buffer_play_info::play_mode::streaming_loop_invalid;
-                //             // playing.callback(playing.callback_data, &playing);
-                //             break;
-                //         }
-                //     }
-                // }
-
                 float sample = buf[frame];
                 for (int channel = 0; channel < channels_count; channel++) {
                     write_sample(areas[channel].ptr, sample);
                     areas[channel].ptr += areas[channel].step;
                 }
             }
-
-            // std::erase_if(thiz->playing_list, [](const auto &playing) {
-            //     return playing.stopped;
-            // });
         }
     };
 
@@ -365,19 +329,9 @@ class audio_libsoundio : public audio_backend {
     }
 
     audio_context_id play_audio_buffer(const audio_buffer_play_info &info) override {
-        // playing_list.push_back(info);
-        // playing_list.back().id = id_serial_count;
-        // id_serial_count++;
         return global_module<audio_mixer>::get().add_playing(info);
     }
     void set_playing_state(audio_context_id id, const audio_buffer_play_info &info) override {
-        // for (auto &playing : playing_list) {
-        //     if (playing.id == id) {
-        //         playing = info;
-        //         playing.id = id;
-        //         break;
-        //     }
-        // }
         global_module<audio_mixer>::get().set_playing(id, info);
     }
 };
