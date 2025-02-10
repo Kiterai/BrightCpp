f19b5a8a973254dd845e28fa77626af3c5bb9d1f
kiterai <kiterai@foolslab.net>
2025-02-10 17:50:07 +0900

    moved audio_buffer_play_info definition

--- src/audio/mixer.hpp
+++ src/audio/mixer.hpp
@@ -1,10 +1,30 @@
-#include "../interfaces/audio.hpp"
 #include <brightcpp/common.hpp>
 #include <vector>
 
 BRIGHTCPP_START
 
 namespace internal {
+
+using audio_context_id = size_t;
+
+struct audio_buffer_play_info {
+    int delay_timer;
+    const float *current_pos, *end_pos, *loop_pos, *next_loop_end_pos;
+    float volume;
+    audio_context_id id;
+    enum class play_mode {
+        normal,
+        oneshot,
+        loop,
+        streaming_loop_invalid,
+        streaming_loop_available,
+    } mode;
+    bool stopped, paused;
+
+    using streaming_callback = void(void *, audio_buffer_play_info *);
+    void *callback_data;
+    streaming_callback callback;
+};
 
 class audio_mixer {
     std::vector<audio_buffer_play_info> playing_list;
--- src/interfaces/audio.hpp
+++ src/interfaces/audio.hpp
@@ -7,27 +7,6 @@ BRIGHTCPP_START
 
 namespace internal {
 
-using audio_context_id = size_t;
-
-struct audio_buffer_play_info {
-    int delay_timer;
-    const float *current_pos, *end_pos, *loop_pos, *next_loop_end_pos;
-    float volume;
-    audio_context_id id;
-    enum class play_mode {
-        normal,
-        oneshot,
-        loop,
-        streaming_loop_invalid,
-        streaming_loop_available,
-    } mode;
-    bool stopped, paused;
-
-    using streaming_callback = void(void *, audio_buffer_play_info*);
-    void *callback_data;
-    streaming_callback callback;
-};
-
 class audio_backend {
   public:
     virtual ~audio_backend() = default;
