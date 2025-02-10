50811a1ba51027098ddfc290d4d5cb2b0753022c
kiterai <kiterai@foolslab.net>
2025-02-10 18:29:32 +0900

    streaming_manager(WIP)

---
+++ src/audio/streaming_manager.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "streaming_manager.hpp"
+
+BRIGHTCPP_START
+
+namespace internal {
+
+
+
+} // namespace internal
+
+BRIGHTCPP_END
---
+++ src/audio/streaming_manager.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <brightcpp/audio.hpp>
+#include <brightcpp/common.hpp>
+
+BRIGHTCPP_START
+
+namespace internal {
+
+} // namespace internal
+
+BRIGHTCPP_END
