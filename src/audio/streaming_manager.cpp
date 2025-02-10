308c5cd08125777c9e58d10da6227b11697bee6b
kiterai <kiterai@foolslab.net>
2025-02-10 20:22:10 +0900

    streaming_manager

--- src/audio/streaming_manager.cpp
+++ src/audio/streaming_manager.cpp
@@ -1,12 +1,46 @@
 #pragma once
 
 #include "streaming_manager.hpp"
+#include "../global_module.hpp"
+#include "mixer.hpp"
+#include <chrono>
 
 BRIGHTCPP_START
 
 namespace internal {
 
+streaming_manager::streaming_manager() {
+    streaming_thread = std::thread([this]() {
+        const auto &default_mixer = global_module<audio_mixer>::get();
 
+        while (running) {
+            for (const auto &ctx : streams)
+                if (default_mixer.get_playing(ctx.id).mode ==
+                    audio_buffer_play_info::play_mode::streaming_loop_invalid)
+                    ctx.loader();
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    });
+}
+
+streaming_manager::~streaming_manager() {
+    running = false;
+    streaming_thread.join();
+}
+
+void streaming_manager::register_loader(audio_context_id id, std::function<void()> callback) {
+    streams.push_back(streaming_context{
+        .id = id,
+        .loader = std::move(callback),
+    });
+}
+
+void streaming_manager::unregister_loader(audio_context_id id) {
+    std::erase_if(streams, [id](const streaming_context &ctx) {
+        return id == ctx.id;
+    });
+}
 
 } // namespace internal
 
--- src/audio/streaming_manager.hpp
+++ src/audio/streaming_manager.hpp
@@ -1,11 +1,30 @@
 #pragma once
 
+#include "mixer.hpp"
 #include <brightcpp/audio.hpp>
 #include <brightcpp/common.hpp>
+#include <functional>
+#include <thread>
 
 BRIGHTCPP_START
 
 namespace internal {
+
+class streaming_manager {
+    struct streaming_context {
+        audio_context_id id;
+        std::function<void()> loader;
+    };
+    std::vector<streaming_context> streams;
+    std::thread streaming_thread;
+    bool running = true;
+
+  public:
+    streaming_manager();
+    ~streaming_manager();
+    void register_loader(audio_context_id id, std::function<void()> callback);
+    void unregister_loader(audio_context_id id);
+};
 
 } // namespace internal
 
