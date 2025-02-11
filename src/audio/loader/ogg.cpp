d615513fefb20d69eb3d44ec5035fd4cd0e4da69
kiterai <kiterai@foolslab.net>
2025-02-11 23:06:49 +0900

    ogg streaming

--- src/audio/loader/ogg.cpp
+++ src/audio/loader/ogg.cpp
@@ -1,4 +1,5 @@
 #include "ogg.hpp"
+#include <iostream>
 #include <stdexcept>
 #include <vorbis/codec.h>
 #include <vorbis/vorbisfile.h>
@@ -8,15 +9,18 @@ BRIGHTCPP_START
 namespace internal {
 
 class oggvorbis_loader : public audio_loader_backend {
-    FILE *fp;
+    FILE *fp = nullptr;
     OggVorbis_File vf;
 
   public:
     ~oggvorbis_loader() override {
+        std::cout << "b" << std::endl;
         ov_clear(&vf);
-        fclose(fp);
+        if (fp)
+            fclose(fp);
     }
     audio_loaded_meta open(std::filesystem::path path) override {
+        std::cout << "a" << std::endl;
         fp = std::fopen(path.string().c_str(), "rb");
 
         if (ov_open(fp, &vf, NULL, 0) < 0)
@@ -46,8 +50,25 @@ class oggvorbis_loader : public audio_loader_backend {
 
         return loaded_audio;
     }
-    // size_t load_chunk(float *out, size_t max_sample) override {
-    // }
+    size_t load_chunk(float *out, size_t max_sample) override {
+        constexpr size_t bufsize = 4096;
+        std::vector<char> buffer(bufsize);
+        auto p = reinterpret_cast<int16_t *>(buffer.data());
+        int cur, decoded;
+        size_t decoded_sum = 0;
+
+        int block_size = 2 * sizeof(int16_t);
+        do {
+            decoded = ov_read(&vf, buffer.data(), std::min(bufsize, (max_sample - decoded_sum) * block_size), 0, 2, 1, &cur);
+            for (int i = 0; i < decoded / block_size; i++) {
+                *out = float(p[i * 2]) / 32768;
+                out++;
+                decoded_sum++;
+            }
+        } while (decoded > 0 && decoded_sum < max_sample);
+
+        return decoded_sum;
+    }
 };
 
 std::unique_ptr<audio_loader_backend> make_oggvorbis_loader() {
