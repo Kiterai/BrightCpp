17972c41ec8c9bc647128c71120c80692ed98af3
kiterai <kiterai@foolslab.net>
2025-02-12 13:05:25 +0900

    streaming resampler

--- src/audio/resample.cpp
+++ src/audio/resample.cpp
@@ -1,7 +1,6 @@
 #include "resample.hpp"
-#include <samplerate.h>
-#include <stdexcept>
 #include <iostream>
+#include <stdexcept>
 
 BRIGHTCPP_START
 
@@ -35,6 +34,42 @@ std::vector<float> instant_full_resample(const std::vector<float> &src, float sr
     src_delete(state);
 
     return dst;
+}
+
+streaming_resampler::streaming_resampler(float in_samplerate, float out_samplerate) : rate{out_samplerate / in_samplerate} {
+    int err;
+    auto state = src_new(SRC_SINC_MEDIUM_QUALITY, 1, &err);
+    if (state == nullptr)
+        throw std::runtime_error("failed to setup resampler: " + std::string(src_strerror(err)));
+}
+
+streaming_resampler::~streaming_resampler() {
+    src_delete(state);
+}
+
+size_t streaming_resampler::resample(float *out, const float *in, size_t in_frames) {
+    for(size_t i = 0; i < in_frames; i++)
+        buffer.push_back(in[i]);
+
+    size_t out_samples = in_frames * rate;
+
+    SRC_DATA data;
+    data.data_in = in;
+    data.input_frames = in_frames;
+    data.data_out = out;
+    data.output_frames = out_samples;
+    data.src_ratio = rate;
+    data.end_of_input = 0;
+
+    auto err = src_process(state, &data);
+    if (err != 0)
+        throw std::runtime_error("failed to process resampling: " + std::string(src_strerror(err)));
+
+    for (size_t i = data.input_frames_used; i < in_frames; i++)
+        buffer[i - data.input_frames_used] = buffer[i];
+    buffer.resize(in_frames - data.input_frames_used);
+
+    return data.output_frames_gen;
 }
 
 } // namespace internal
--- src/audio/resample.hpp
+++ src/audio/resample.hpp
@@ -1,4 +1,5 @@
 #include <brightcpp/common.hpp>
+#include <samplerate.h>
 #include <vector>
 
 BRIGHTCPP_START
@@ -6,6 +7,18 @@ BRIGHTCPP_START
 namespace internal {
 
 std::vector<float> instant_full_resample(const std::vector<float> &src, float src_samplerate, float dst_samplerate);
+
+class streaming_resampler {
+    SRC_STATE *state;
+    float rate;
+    std::vector<float> buffer;
+
+  public:
+    streaming_resampler(float in_samplerate, float out_samplerate);
+    ~streaming_resampler();
+
+    size_t resample(float *out, const float *in, size_t in_frames);
+};
 
 } // namespace internal
 
