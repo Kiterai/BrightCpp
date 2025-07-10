e9a172c2d346779c40c0e9818f467e371ebd11e4
kiterai <kiterai@foolslab.net>
2025-07-10 10:02:28 +0900

    change vbuffer interface

--- src/graphics/vulkan/vbuffer.hpp
+++ src/graphics/vulkan/vbuffer.hpp
@@ -19,8 +19,8 @@ class vbuffer_factory_vulkan : public vbuffer_factory_backend {
     vbuffer_factory_vulkan();
     ~vbuffer_factory_vulkan();
 
-    handle_holder<image_impl>::handle_value_t make(const uint8_t *data, size_t bytes_num) override;
-    void destroy(const handle_holder<image_impl> &) noexcept override;
+    handle_holder<vbuffer_impl>::handle_value_t make(const uint8_t *data, size_t bytes_num) override;
+    void destroy(const handle_holder<vbuffer_impl> &) noexcept override;
 };
 
 BRIGHTCPP_GRAPHICS_VULKAN_END
--- src/interfaces/vbuffer_factory.hpp
+++ src/interfaces/vbuffer_factory.hpp
@@ -10,12 +10,14 @@ BRIGHTCPP_START
 
 namespace internal {
 
+class vbuffer_impl;
+
 class vbuffer_factory_backend {
   public:
     virtual ~vbuffer_factory_backend() = default;
 
-    virtual handle_holder<image_impl>::handle_value_t make(const uint8_t *data, size_t bytes_num) = 0;
-    virtual void destroy(const handle_holder<image_impl> &) noexcept = 0;
+    virtual handle_holder<vbuffer_impl>::handle_value_t make(const uint8_t *data, size_t bytes_num) = 0;
+    virtual void destroy(const handle_holder<vbuffer_impl> &) noexcept = 0;
 };
 
 } // namespace internal
