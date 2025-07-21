8a9fea8f1f10d42b6590912e6636887e746450c9
kiterai <kiterai@foolslab.net>
2025-07-21 18:29:15 +0900

    entity_manager

--- include/brightcpp/handle_holder.hpp
+++ include/brightcpp/handle_holder.hpp
@@ -5,10 +5,12 @@
 
 BRIGHTCPP_START
 
+using entity_handle_t = std::size_t;
+
 template <class T>
 class handle_holder {
   public:
-    using handle_value_t = std::size_t;
+    using handle_value_t = entity_handle_t;
     handle_value_t handle() const { return handle_value; }
 
   private:
---
+++ src/entity_holder.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <brightcpp/handle_holder.hpp>
+#include <unordered_map>
+
+BRIGHTCPP_START
+
+namespace internal {
+
+template <class T> class entity_holder {
+    size_t id_count = 0;
+    std::unordered_map<entity_handle_t, T> db;
+
+  public:
+    template <class... Args> entity_handle_t make(Args &&...args) {
+        auto new_id = id_count;
+        db.emplace(std::piecewise_construct, std::forward_as_tuple(new_id), std::forward_as_tuple(args...));
+        id_count++;
+
+        return new_id;
+    }
+    void destroy(entity_handle_t handle) { db.erase(handle); }
+    T &get(entity_handle_t handle) { return db.at(handle); }
+    const T &get(entity_handle_t handle) const { return db.at(handle); }
+};
+
+} // namespace internal
+
+BRIGHTCPP_END
