cc7878cd136986bae25ad1279d603226ce51b6df
kiterai <kiterai@foolslab.net>
2025-07-22 00:09:39 +0900

    used entity_holder

--- src/graphics/vulkan/rendertarget_factory.cpp
+++ src/graphics/vulkan/rendertarget_factory.cpp
@@ -10,48 +10,31 @@ BRIGHTCPP_GRAPHICS_VULKAN_START
 
 rendertarget_factory_vulkan::~rendertarget_factory_vulkan() { global_module<graphics_vulkan>::get().wait_idle(); }
 
-handle_holder<rendertarget>::handle_value_t rendertarget_factory_vulkan::create_render_target(window_backend &window) {
-    const auto handle = rendertarget_db.size();
-
+entity_handle_t rendertarget_factory_vulkan::create_render_target(window_backend &window) {
     const auto &g = global_module<graphics_vulkan>::get();
 
-    rendertarget_db.insert({
-        handle,
-        std::make_unique<window_rendertarget_vulkan>(g.get_instance(), g.get_physdevice(), g.get_device(),
-                                                     g.get_queue_indices(),
-                                                     window.get_vulkan_surface(g.get_instance())),
-    });
-    return handle;
+    return rendertarget_db.make(std::make_unique<window_rendertarget_vulkan>(
+        g.get_instance(), g.get_physdevice(), g.get_device(), g.get_queue_indices(),
+        window.get_vulkan_surface(g.get_instance())));
 }
 
-handle_holder<rendertarget>::handle_value_t
-rendertarget_factory_vulkan::create_render_target(handle_holder<image_impl> image) {
-    const auto handle = rendertarget_db.size();
-
+entity_handle_t rendertarget_factory_vulkan::create_render_target(handle_holder<image_impl> image) {
     const auto &g = global_module<graphics_vulkan>::get();
 
     const auto &tex = global_module<texture_factory_vulkan>::get().get(image);
 
-    rendertarget_db.insert({
-        handle,
-        std::make_unique<texture_rendertarget_vulkan>(
-            g.get_device(),
-            tex.format,
-            vk::Extent2D(tex.w_int, tex.h_int),
-            std::vector<vk::Image>{ tex.image.get() },
-            g.get_queue_indices()
-        ),
-    });
-    return handle;
+    return rendertarget_db.make(
+        std::make_unique<texture_rendertarget_vulkan>(g.get_device(), tex.format, vk::Extent2D(tex.w_int, tex.h_int),
+                                                      std::vector<vk::Image>{tex.image.get()}, g.get_queue_indices()));
 }
 
 void rendertarget_factory_vulkan::destroy_render_target(handle_holder<rendertarget> &rt) noexcept {
-    rendertarget_db.erase(rt.handle());
+    rendertarget_db.destroy(rt.handle());
 }
 
 abstract_rendertarget_vulkan &
 rendertarget_factory_vulkan::get_render_target_vulkan(handle_holder<rendertarget> handle) {
-    return *rendertarget_db.at(handle.handle()).get();
+    return *rendertarget_db.get(handle.handle()).get();
 }
 
 BRIGHTCPP_GRAPHICS_VULKAN_END
--- src/graphics/vulkan/rendertarget_factory.hpp
+++ src/graphics/vulkan/rendertarget_factory.hpp
@@ -1,22 +1,20 @@
 #pragma once
 
+#include "../../entity_holder.hpp"
 #include "../../interfaces/rendertarget_factory.hpp"
 #include "rendertarget.hpp"
 #include "vulkan_common.hpp"
-#include <unordered_map>
 
 BRIGHTCPP_GRAPHICS_VULKAN_START
 
 class rendertarget_factory_vulkan : public rendertarget_factory_backend {
-    using handle_t = handle_holder<rendertarget>;
-    using handle_value_t = handle_t::handle_value_t;
-    std::unordered_map<handle_value_t, std::unique_ptr<vulkan::abstract_rendertarget_vulkan>> rendertarget_db;
+    entity_holder<std::unique_ptr<vulkan::abstract_rendertarget_vulkan>> rendertarget_db;
 
   public:
     ~rendertarget_factory_vulkan() override;
 
-    handle_holder<rendertarget>::handle_value_t create_render_target(window_backend &window) override;
-    handle_holder<rendertarget>::handle_value_t create_render_target(handle_holder<image_impl> image) override;
+    entity_handle_t create_render_target(window_backend &window) override;
+    entity_handle_t create_render_target(handle_holder<image_impl> image) override;
     void destroy_render_target(handle_holder<rendertarget> &rt) noexcept override;
     abstract_rendertarget_vulkan &get_render_target_vulkan(handle_holder<rendertarget> handle);
 };
