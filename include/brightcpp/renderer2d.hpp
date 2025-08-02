1bb050e2c8cbb572c5e12c6f6e02e4bd5d112078
kiterai <kiterai@foolslab.net>
2025-08-02 23:50:36 +0900

    added const

--- include/brightcpp/renderer2d.hpp
+++ include/brightcpp/renderer2d.hpp
@@ -20,23 +20,23 @@ class renderer2d : handle_holder<renderer2d> {
 
     template <class T> renderer2d(T &renderable) : renderer2d(renderable.get_render_target()) {}
 
-    renderer2d &draw(image &image, int x, int y);
-    renderer2d &draw(image_clip &clip, int x, int y);
-    renderer2d &draw(image &image, int x, int y, float rotation);
-    renderer2d &draw(image_clip &clip, int x, int y, float rotation);
-    renderer2d &draw(image &image, int x, int y, float scale_x, float scale_y);
-    renderer2d &draw(image_clip &clip, int x, int y, float scale_x, float scale_y);
-    renderer2d &draw(image &image, int x, int y, float rotation, float scale_x, float scale_y);
-    renderer2d &draw(image_clip &clip, int x, int y, float rotation, float scale_x, float scale_y);
+    renderer2d &draw(const image &image, int x, int y);
+    renderer2d &draw(const image_clip &clip, int x, int y);
+    renderer2d &draw(const image &image, int x, int y, float rotation);
+    renderer2d &draw(const image_clip &clip, int x, int y, float rotation);
+    renderer2d &draw(const image &image, int x, int y, float scale_x, float scale_y);
+    renderer2d &draw(const image_clip &clip, int x, int y, float scale_x, float scale_y);
+    renderer2d &draw(const image &image, int x, int y, float rotation, float scale_x, float scale_y);
+    renderer2d &draw(const image_clip &clip, int x, int y, float rotation, float scale_x, float scale_y);
 
-    renderer2d &draw(image &image, int x, int y, pivot image_pivot);
-    renderer2d &draw(image_clip &clip, int x, int y, pivot clip_pivot);
-    renderer2d &draw(image &image, int x, int y, float rotation, pivot image_pivot);
-    renderer2d &draw(image_clip &clip, int x, int y, float rotation, pivot clip_pivot);
-    renderer2d &draw(image &image, int x, int y, float scale_x, float scale_y, pivot clip_pivot);
-    renderer2d &draw(image_clip &clip, int x, int y, float scale_x, float scale_y, pivot clip_pivot);
-    renderer2d &draw(image &image, int x, int y, float rotation, float scale_x, float scale_y, pivot clip_pivot);
-    renderer2d &draw(image_clip &clip, int x, int y, float rotation, float scale_x, float scale_y, pivot clip_pivot);
+    renderer2d &draw(const image &image, int x, int y, pivot image_pivot);
+    renderer2d &draw(const image_clip &clip, int x, int y, pivot clip_pivot);
+    renderer2d &draw(const image &image, int x, int y, float rotation, pivot image_pivot);
+    renderer2d &draw(const image_clip &clip, int x, int y, float rotation, pivot clip_pivot);
+    renderer2d &draw(const image &image, int x, int y, float scale_x, float scale_y, pivot clip_pivot);
+    renderer2d &draw(const image_clip &clip, int x, int y, float scale_x, float scale_y, pivot clip_pivot);
+    renderer2d &draw(const image &image, int x, int y, float rotation, float scale_x, float scale_y, pivot clip_pivot);
+    renderer2d &draw(const image_clip &clip, int x, int y, float rotation, float scale_x, float scale_y, pivot clip_pivot);
 
     renderer2d &draw_filled_rect(color fill_color, int x, int y, int width, int height);
 
--- src/graphics/renderer2d.cpp
+++ src/graphics/renderer2d.cpp
@@ -11,57 +11,57 @@ using g_renderer_middleware_manager_t = internal::global_module<internal::render
 renderer2d::renderer2d(rendertarget rt) : handle_holder{g_renderer_middleware_manager_t::get().make(rt)} {}
 renderer2d::~renderer2d() { g_renderer_middleware_manager_t::get().destroy(handle()); };
 
-renderer2d &renderer2d::draw(image &image, int x, int y) {
+renderer2d &renderer2d::draw(const image &image, int x, int y) {
     return draw(image, x, y, 0.0f, 1.0f, 1.0f, pivot_pos::left_top);
 }
-renderer2d &renderer2d::draw(image_clip &clip, int x, int y) {
+renderer2d &renderer2d::draw(const image_clip &clip, int x, int y) {
     return draw(clip, x, y, 0.0f, 1.0f, 1.0f, pivot_pos::left_top);
 }
-renderer2d &renderer2d::draw(image &image, int x, int y, float rotation) {
+renderer2d &renderer2d::draw(const image &image, int x, int y, float rotation) {
     return draw(image, x, y, rotation, 1.0f, 1.0f, pivot_pos::left_top);
 }
-renderer2d &renderer2d::draw(image_clip &clip, int x, int y, float rotation) {
+renderer2d &renderer2d::draw(const image_clip &clip, int x, int y, float rotation) {
     return draw(clip, x, y, rotation, 1.0f, 1.0f, pivot_pos::left_top);
 }
-renderer2d &renderer2d::draw(image &image, int x, int y, float scale_x, float scale_y) {
+renderer2d &renderer2d::draw(const image &image, int x, int y, float scale_x, float scale_y) {
     return draw(image, x, y, 0.0f, scale_x, scale_y, pivot_pos::left_top);
 }
-renderer2d &renderer2d::draw(image_clip &clip, int x, int y, float scale_x, float scale_y) {
+renderer2d &renderer2d::draw(const image_clip &clip, int x, int y, float scale_x, float scale_y) {
     return draw(clip, x, y, 0.0f, scale_x, scale_y, pivot_pos::left_top);
 }
-renderer2d &renderer2d::draw(image &image, int x, int y, float rotation, float scale_x, float scale_y) {
+renderer2d &renderer2d::draw(const image &image, int x, int y, float rotation, float scale_x, float scale_y) {
     return draw(image, x, y, rotation, scale_x, scale_y, pivot_pos::left_top);
 }
-renderer2d &renderer2d::draw(image_clip &clip, int x, int y, float rotation, float scale_x, float scale_y) {
+renderer2d &renderer2d::draw(const image_clip &clip, int x, int y, float rotation, float scale_x, float scale_y) {
     return draw(clip, x, y, rotation, scale_x, scale_y, pivot_pos::left_top);
 }
 
-renderer2d &renderer2d::draw(image &image, int x, int y, pivot image_pivot) {
+renderer2d &renderer2d::draw(const image &image, int x, int y, pivot image_pivot) {
     return draw(image, x, y, 0.0f, 1.0f, 1.0f, image_pivot);
 }
-renderer2d &renderer2d::draw(image_clip &clip, int x, int y, pivot clip_pivot) {
+renderer2d &renderer2d::draw(const image_clip &clip, int x, int y, pivot clip_pivot) {
     return draw(clip, x, y, 0.0f, 1.0f, 1.0f, clip_pivot);
 }
-renderer2d &renderer2d::draw(image &image, int x, int y, float rotation, pivot image_pivot) {
+renderer2d &renderer2d::draw(const image &image, int x, int y, float rotation, pivot image_pivot) {
     return draw(image, x, y, rotation, 1.0f, 1.0f, image_pivot);
 }
-renderer2d &renderer2d::draw(image_clip &clip, int x, int y, float rotation, pivot clip_pivot) {
+renderer2d &renderer2d::draw(const image_clip &clip, int x, int y, float rotation, pivot clip_pivot) {
     return draw(clip, x, y, rotation, 1.0f, 1.0f, clip_pivot);
 }
-renderer2d &renderer2d::draw(image &image, int x, int y, float scale_x, float scale_y, pivot image_pivot) {
+renderer2d &renderer2d::draw(const image &image, int x, int y, float scale_x, float scale_y, pivot image_pivot) {
     return draw(image, x, y, 0.0f, scale_x, scale_y, image_pivot);
 }
-renderer2d &renderer2d::draw(image_clip &clip, int x, int y, float scale_x, float scale_y, pivot clip_pivot) {
+renderer2d &renderer2d::draw(const image_clip &clip, int x, int y, float scale_x, float scale_y, pivot clip_pivot) {
     return draw(clip, x, y, 0.0f, scale_x, scale_y, clip_pivot);
 }
 
-renderer2d &renderer2d::draw(image &image, int x, int y, float rotation, float scale_x, float scale_y,
+renderer2d &renderer2d::draw(const image &image, int x, int y, float rotation, float scale_x, float scale_y,
                              pivot image_pivot) {
     g_renderer_middleware_manager_t::get().get(handle()).draw_texture(image, x, y, rotation, scale_x, scale_y,
                                                                       image_pivot);
     return *this;
 }
-renderer2d &renderer2d::draw(image_clip &clip, int x, int y, float rotation, float scale_x, float scale_y,
+renderer2d &renderer2d::draw(const image_clip &clip, int x, int y, float rotation, float scale_x, float scale_y,
                              pivot clip_pivot) {
     g_renderer_middleware_manager_t::get().get(handle()).draw_texture(clip, x, y, rotation, scale_x, scale_y,
                                                                       clip_pivot);
